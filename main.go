709cc90f45e9deef439683f2161809bb1ee8eb78
lingengyuan <20433276+lingengyuan@users.noreply.github.com>
2026-08-27 14:24:52 +0800

    chore: prepare v0.0.1 release

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-var version = "0.4.0"
+var version = "0.0.1"
 
 const defaultNetworkTimeout = 10 * time.Second
 
--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-    [string]$Version = "0.4.0"
+    [string]$Version = "0.0.1"
 )
 
 $ErrorActionPreference = "Stop"
